66a457342ab0fe88f88f896b52d57fbf3a531621
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:16:02 +0530

    Add files via upload

---
+++ character.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.*;
+public class character {
+	public static void main(String args[]) {
+		char ch;
+		Scanner inp = new Scanner(System.in);
+		int a = inp.nextInt();
+		for(int i=1;i<=a;i++,System.out.println()) {
+		for(ch='a';ch<='z';ch++) {
+			System.out.print(ch);
+			
+		}
+		}
+	}
+
+}
