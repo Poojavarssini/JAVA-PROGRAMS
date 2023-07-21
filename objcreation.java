3962a78070179a10d637ff03a549a1e99b61724a
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:34:54 +0530

    Add files via upload

---
+++ objcreation.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.*;
+class value{
+	int a;
+}
+public class objcreation {
+	public static void main(String args[]) {
+		value inp = new value();
+		inp.a = 5;
+		System.out.println(inp.a);
+	}
+
+}
