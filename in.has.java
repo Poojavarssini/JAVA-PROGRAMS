1fe2a84d1486ebf8269bdafb646397c3c6b76320
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:27:50 +0530

    Add files via upload

---
+++ in.has.java
@@ -0,0 +1,16 @@
+package practice;
+import java.util.*;
+public class ghxdhg {
+
+	    public static void main(String[] args) {
+	       Scanner in = new Scanner(System.in);
+	      
+	       for(int i=1;in.hasNext()== true;i++){
+	           System.out.println(i+ " "+in.nextLine());
+	       }
+	       
+	       
+	    }
+	}
+
+
