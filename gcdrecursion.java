b70b4a2aa2d9b62d64d7a204d29041d0e0f61d28
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:25:42 +0530

    Add files via upload

---
+++ gcdrecursion.java
@@ -0,0 +1,25 @@
+
+package practice;
+import java.util.*;
+
+ public class gcdrecursion{
+	 public static void main(String args[]) {
+		 Scanner inp=new Scanner(System.in);
+		 int num = inp.nextInt();
+		 System.out.println(num<<1);
+	 }
+ }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
