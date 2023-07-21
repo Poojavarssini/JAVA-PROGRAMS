1ecb244db1ea408c97c00be91ec5e07d9e5c646c
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:22:48 +0530

    Add files via upload

---
+++ empdetails.java
@@ -0,0 +1,17 @@
+package practice;
+import java.util.*;
+public class empdetails {
+	Scanner sc=new Scanner(System.in);
+	//
+	int hours=sc.nextInt();
+}
+class get_info{
+	public static void main(String[]args)
+	{
+	empdetails details=new empdetails();
+	int salary;
+	salary=details.hours*5;
+	System.out.println(salary);
+	
+}
+}
