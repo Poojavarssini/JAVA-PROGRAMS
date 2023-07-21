7c5d5969893122326136e78c0a31f1f1ba376768
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:16:24 +0530

    Add files via upload

---
+++ charcount.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.*;
+public class charcount {
+	public static void main(String args[]) {
+		Scanner inp = new Scanner(System.in);
+		String str = inp.nextLine();
+		int count =0;
+		for(int i=0;i<str.length();i++) {
+			count++;
+			
+		}
+		System.out.print(count);
+	}
+
+}
