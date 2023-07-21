98d67d64cb000d74b4c266c6a40db777eddc1845
Poojavarssini <133085270+Poojavarssini@users.noreply.github.com>
2023-07-21 14:36:16 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,21 @@
+package practice;
+
+import java.util.*;
+public class pattern {
+	public static void main(String args[]) {
+		Scanner inp = new Scanner(System.in);
+		int a = inp.nextInt();
+		int cnt=1;
+		for(int i=1;i<=a;i++) {
+			for(int j=1;j<i;j++) {
+				System.out.print(cnt+" ");
+				cnt++;
+				
+			}
+		System.out.println("");
+		System.out.println();
+			}
+		
+	}
+	}
+	
